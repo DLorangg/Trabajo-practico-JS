73a0695626b147699f99ed7d35cf00541daea4ae
Damian Lorang <lorang.damian.epet20@gmail.com>
2022-09-29 22:56:36 -0300

    3 console.log para las variables suma, resta y multitplicacion

--- script.js
+++ script.js
@@ -12,4 +12,7 @@ let multiplicacion = num1*num2;
 console.log(Damian);
 console.log(Wiedermann);
 console.log(Juan);
+console.log(suma);
+console.log(resta);
+console.log(multiplicacion);
 //#endregion impresion
