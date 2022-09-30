d5bf6be7e91e18da6fe08e7bcf03da23943ae011
Damian Lorang <lorang.damian.epet20@gmail.com>
2022-09-29 22:45:44 -0300

    Agregamos valor a la variable num1

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 var Damian = `Alumno comprometido`;
 let Wiedermann = 'El profe que mejor explica';
 const Juan = "Preceptor que no suele faltar";
+let num1 = 12;
 //#endregion variables
 //#region impresion
 console.log(Damian);
