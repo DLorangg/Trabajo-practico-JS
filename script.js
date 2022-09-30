2906d55ef47e52a3eb3938df8f4590703ab0c90c
Damian Lorang <lorang.damian.epet20@gmail.com>
2022-09-29 22:48:13 -0300

    Creamos la variable 'suma' que suma las variables num1 y num2

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let Wiedermann = 'El profe que mejor explica';
 const Juan = "Preceptor que no suele faltar";
 let num1 = 12;
 let num2 = 38;
+let suma = num1+num2;
 //#endregion variables
 //#region impresion
 console.log(Damian);
