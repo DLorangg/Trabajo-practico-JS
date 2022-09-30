2d2d209dd6e61c7767c3a1fd53a5c0a5be78bc3a
Damian Lorang <lorang.damian.epet20@gmail.com>
2022-09-29 22:41:49 -0300

    Agregamos console.log para los 3 tipos de variables

--- script.js
+++ script.js
@@ -4,5 +4,7 @@ let Wiedermann = 'El profe que mejor explica';
 const Juan = "Preceptor que no suele faltar";
 //#endregion variables
 //#region impresion
-
+console.log(Damian);
+console.log(Wiedermann);
+console.log(Juan);
 //#endregion impresion
